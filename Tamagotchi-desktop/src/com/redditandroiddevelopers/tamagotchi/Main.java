5f5648b74a041a1da24132e0dbb6c508598af49d
Frederick Maasch <member68@googlemail.com>
2012-04-16 08:57:58 +0200

    Updated desktop screen resolution to reflect HDPI devices

--- Tamagotchi-desktop/src/com/redditandroiddevelopers/tamagotchi/Main.java
+++ Tamagotchi-desktop/src/com/redditandroiddevelopers/tamagotchi/Main.java
@@ -9,8 +9,8 @@ public class Main {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "Tamagotchi";
 		cfg.useGL20 = false;
-		cfg.width = 480;
-		cfg.height = 320;
+		cfg.width = 800;
+		cfg.height = 480;
 		
 		new LwjglApplication(new TamagotchiGame(), cfg);
 	}
