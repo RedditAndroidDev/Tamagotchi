20fae3c1ac317f4331eef7a79a4341b3cfb12deb
Frederick Maasch <member68@googlemail.com>
2012-04-16 13:14:44 +0200

    Prevent resizing the desktop app

--- Tamagotchi-desktop/src/com/redditandroiddevelopers/tamagotchi/Main.java
+++ Tamagotchi-desktop/src/com/redditandroiddevelopers/tamagotchi/Main.java
@@ -11,7 +11,8 @@ public class Main {
         cfg.useGL20 = false;
         cfg.width = 800;
         cfg.height = 480;
-
+        cfg.resizable = false;
+        
         new LwjglApplication(new TamagotchiGame(), cfg);
     }
 }
