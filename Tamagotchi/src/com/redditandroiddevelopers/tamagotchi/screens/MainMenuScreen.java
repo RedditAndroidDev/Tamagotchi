1009b0b3fbc85a4616a7eb7ce37cdfd43ffb6fe3
Jack Patrick <jackpatrickmail@gmail.com>
2012-04-16 13:23:35 +0100

    Added Button class

--- Tamagotchi/src/com/redditandroiddevelopers/tamagotchi/screens/MainMenuScreen.java
+++ Tamagotchi/src/com/redditandroiddevelopers/tamagotchi/screens/MainMenuScreen.java
@@ -1,16 +1,25 @@
 package com.redditandroiddevelopers.tamagotchi.screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.redditandroiddevelopers.tamagotchi.ui.Button;
+
 public class MainMenuScreen extends CommonScreen {
 
+	Button button = new Button("button-background.png", 0, 0, 2);
+	
+	SpriteBatch batch = new SpriteBatch();
+	Stage stage;
     @Override
     public void update(float delta) {
-        // TODO Auto-generated method stub
+		 stage = new Stage(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), false, batch);
+		 stage.addActor(button);
     }
 
     @Override
     public void draw(float delta) {
-        // TODO Auto-generated method stub
-
+    	stage.draw();
     }
 
 }
