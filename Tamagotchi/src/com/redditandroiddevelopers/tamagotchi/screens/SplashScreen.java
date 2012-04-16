b5578a76262edc1c017c319e37671a1aad88f08d
Frederick Maasch <member68@googlemail.com>
2012-04-16 12:10:34 +0200

    Added documentation and comments to SplashScreen

--- Tamagotchi/src/com/redditandroiddevelopers/tamagotchi/screens/SplashScreen.java
+++ Tamagotchi/src/com/redditandroiddevelopers/tamagotchi/screens/SplashScreen.java
@@ -17,6 +17,11 @@ public class SplashScreen extends CommonScreen
     private Texture splashTexture;
     private float timeElapsed = 0;
 
+    /**
+     * Creates a default splash screen.
+     * 
+     * @param g Main game instance
+     */
     public SplashScreen(Game g)
     {
         TamagotchiGame = g;
@@ -25,17 +30,27 @@ public class SplashScreen extends CommonScreen
     @Override
     public void show() {
         // super.show();
+
+        // use white background for now
         Gdx.gl.glClearColor(1, 1, 1, 1);
+
         batch = new SpriteBatch();
+
+        // load Reddit alien texture
         splashTexture = new Texture(Gdx.files.internal("Reddit-alien.png"));
     }
 
     @Override
     public void update(float delta) {
+        // delta is the time since the last update, adding it up gives us the
+        // time since the first update
+
         if (timeElapsed < 3) {
             timeElapsed += delta;
         }
         else {
+            // after 3 seconds, the Splash screen is hidden and the
+            // MainMenuScreen is shown
             TamagotchiGame.setScreen(new MainMenuScreen());
         }
     }
@@ -43,14 +58,29 @@ public class SplashScreen extends CommonScreen
     @Override
     public void draw(float delta) {
         batch.begin();
+        // draw Reddit alien on the screen
         batch.draw(splashTexture, getCenterX(splashTexture), getCenterY(splashTexture));
         batch.end();
     }
 
+    /**
+     * Gets the X coordinate to display a texture centered on the X axis of the
+     * screen.
+     * 
+     * @param t Texture to display
+     * @return X coordinate
+     */
     protected int getCenterX(Texture t) {
         return (Gdx.graphics.getWidth() - t.getWidth()) / 2;
     }
 
+    /**
+     * Gets the Y coordinate to display a texture centered on the Y axis of the
+     * screen.
+     * 
+     * @param t Texture to display
+     * @return Y coordinate
+     */
     protected int getCenterY(Texture t) {
         return (Gdx.graphics.getHeight() - t.getHeight()) / 2;
     }
