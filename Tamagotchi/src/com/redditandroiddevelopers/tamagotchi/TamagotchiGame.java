25d0d33e20361d4764a97738660678e8bcf64171
Santoso Wijaya <santoso.wijaya@gmail.com>
2012-04-16 00:28:50 -0700

    Rename MyGame -> TamagotchiGame.

--- Tamagotchi/src/com/redditandroiddevelopers/tamagotchi/TamagotchiGame.java
+++ Tamagotchi/src/com/redditandroiddevelopers/tamagotchi/TamagotchiGame.java
@@ -1,33 +1,60 @@
 package com.redditandroiddevelopers.tamagotchi;
 
-import com.badlogic.gdx.ApplicationListener;
+
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL10;
+import com.redditandroiddevelopers.tamagotchi.screens.CommonScreen;
+import com.redditandroiddevelopers.tamagotchi.screens.GameLoop;
+import com.redditandroiddevelopers.tamagotchi.screens.MainMenuScreen;
+import com.redditandroiddevelopers.tamagotchi.screens.PauseScreen;
 
-public class TamagotchiGame implements ApplicationListener {
-	@Override
-	public void create() {
-	}
+public class TamagotchiGame extends Game {
 
-	@Override
-	public void dispose() {
-	}
+    // enums are expensive
+    public static int STATE_MAIN_MENU = 0;
+    public static int STATE_RUNNING = 1;
+    public static int STATE_PAUSED = 2;
 
-	@Override
-	public void render() {
-		Gdx.gl.glClearColor(1, 0, 1, 1);
-		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-	}
+    public static int MY_STATE = 0;
+    public static boolean STATE_CHANGE = false;
 
-	@Override
-	public void resize(int width, int height) {
-	}
+    @Override
+    public void create() {
+        setScreen(new MainMenuScreen());
+    }
 
-	@Override
-	public void pause() {
-	}
+    @Override
+    public void render() {
+        if (!STATE_CHANGE) {
+            CommonScreen currentScreen = getScreen();
+            currentScreen.render(Gdx.graphics.getDeltaTime());
+        } else {
+            STATE_CHANGE = false;
+            getScreen().dispose();
+            
+            switch (MY_STATE) {
+            case 0:
+                setScreen(new MainMenuScreen());
+            case 1:
+                setScreen(new GameLoop());
+                break;
+            case 2:
+                setScreen(new PauseScreen());
+                break;
+            default:
+                break;
+            }
+        }
+    }
 
-	@Override
-	public void resume() {
-	}
+    @Override
+    public CommonScreen getScreen() {
+        return (CommonScreen) super.getScreen();
+    }
+    
+    public void updateState(int newStateVal) {
+        MY_STATE = newStateVal;
+        STATE_CHANGE = true;
+    }
+
 }
