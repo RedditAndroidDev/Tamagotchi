81f266db46cfcaa4c17dceb0581d0f4b0fb5f9d6
Frederick Maasch <member68@googlemail.com>
2012-04-16 11:14:14 +0200

    Fixed code style

--- Tamagotchi-desktop/src/com/redditandroiddevelopers/tamagotchi/Main.java
+++ Tamagotchi-desktop/src/com/redditandroiddevelopers/tamagotchi/Main.java
@@ -1,16 +1,17 @@
+
 package com.redditandroiddevelopers.tamagotchi;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class Main {
-	public static void main(String[] args) {
-		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "Tamagotchi";
-		cfg.useGL20 = false;
-		cfg.width = 800;
-		cfg.height = 480;
-		
-		new LwjglApplication(new TamagotchiGame(), cfg);
-	}
+    public static void main(String[] args) {
+        LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+        cfg.title = "Tamagotchi";
+        cfg.useGL20 = false;
+        cfg.width = 800;
+        cfg.height = 480;
+
+        new LwjglApplication(new TamagotchiGame(), cfg);
+    }
 }
