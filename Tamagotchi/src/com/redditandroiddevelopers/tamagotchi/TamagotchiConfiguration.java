9b496ac18ca14790146de224c13acc845709f3d4
Santoso Wijaya <santoso.wijaya@gmail.com>
2012-04-19 11:31:10 -0700

    Add javadoc comment for TamagotchiConfiguration.

--- Tamagotchi/src/com/redditandroiddevelopers/tamagotchi/TamagotchiConfiguration.java
+++ Tamagotchi/src/com/redditandroiddevelopers/tamagotchi/TamagotchiConfiguration.java
@@ -3,6 +3,13 @@ package com.redditandroiddevelopers.tamagotchi;
 
 import com.badlogic.gdx.Application;
 
+/**
+ * A place to store runtime configuration for use throughout the lifetime of a
+ * {@link TamagotchiGame} object. Not to be confused with a store for game
+ * settings.
+ * 
+ * @author Santoso Wijaya
+ */
 public class TamagotchiConfiguration {
 
     public boolean debug = true;
